9496403ba04bb57063258537e1cbf2381ea208d0
Roy McFarland <roy.mcfarland@gmail.com>
2015-07-07 09:03:33 -0600

    instantiating strings in ruby

---
+++ lesson3.rb
@@ -0,0 +1,13 @@
+# STRINGS
+s = String.new("Hello world")
+puts s
+
+str1 = 'Hello world'
+puts str1
+
+str2 = "Hello world"
+puts str2
+
+name = "Roy"
+puts "Hello #{name}"
+puts 'Hello #{name}' # no interpolation
